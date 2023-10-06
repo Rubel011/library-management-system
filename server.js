910e351d5906ade16b2a2eeba373af65397a36c8
Rubel Foridi <rubelforidi@gmail.com>
2023-10-06 11:25:58 +0530

    initial setup , folder structure and all dependencies

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./configs/swaggerConfig');
+const { connection } = require('./configs/db');
+const { successResponse, errorResponse } = require('./helpers/successAndError');
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Middleware for cors error
+app.use(cors());
+
+// Serve Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+// Home route
+app.get("/", async (req, res) => {
+    try {
+        res.status(200).json(successResponse(200, "This is the home page running successfully", null));
+    } catch (error) {
+        res.status(500).json(errorResponse(500, error.message));
+    }
+});
+
+
+// Start the server
+app.listen(port, async () => {
+    try {
+        // Establish database connection
+        await connection;
+
+        // Server listening message
+        console.log({ message: `Server is listening on port ${port}` });
+    } catch (error) {
+        console.log(error.message);
+    }
+});
